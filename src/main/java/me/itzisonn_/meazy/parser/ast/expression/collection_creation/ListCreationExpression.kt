623733c0f91921c2c9810948f869de2f79a5799d
ItzIsonn_ <kotov.e2473@gmail.com>
2026-06-11 17:06:40 +0300

    Converted parser to Kotlin #7

--- src/main/java/me/itzisonn_/meazy/parser/ast/expression/collection_creation/ListCreationExpression.kt
+++ src/main/java/me/itzisonn_/meazy/parser/ast/expression/collection_creation/ListCreationExpression.kt
@@ -1,69 +1,48 @@
-package me.itzisonn_.meazy.parser.ast.expression.collection_creation;
+package me.itzisonn_.meazy.parser.ast.expression.collection_creation
 
-import kotlin.Unit;
-import lombok.Getter;
-import me.itzisonn_.meazy.instruction.InstructionsSet;
-import me.itzisonn_.meazy.instruction.method.InvokeMethodInstruction.InvokeType;
-import me.itzisonn_.meazy.parser.ast.ProgramUnit;
-import me.itzisonn_.meazy.parser.ast.expression.Expression;
-import me.itzisonn_.meazy.parser.DataType;
-import me.itzisonn_.meazy.runtime.environment.Environment;
-import org.jspecify.annotations.NullMarked;
+import me.itzisonn_.meazy.instruction.InstructionsSet
+import me.itzisonn_.meazy.instruction.method.InvokeMethodInstruction.InvokeType
+import me.itzisonn_.meazy.parser.DataType
+import me.itzisonn_.meazy.parser.ast.ProgramUnit
+import me.itzisonn_.meazy.parser.ast.expression.Expression
+import me.itzisonn_.meazy.runtime.environment.Environment
+import java.lang.constant.ClassDesc
+import java.lang.constant.ConstantDescs
+import java.lang.constant.MethodTypeDesc
 
-import java.lang.constant.ClassDesc;
-import java.lang.constant.ConstantDescs;
-import java.lang.constant.MethodTypeDesc;
-import java.util.List;
-
-@Getter
-@NullMarked
-public class ListCreationExpression implements Expression {
-    private final List<Expression> list;
-
-    public ListCreationExpression(List<Expression> list) {
-        this.list = list;
-    }
-
-    @Override
-    public void emit(InstructionsSet instructions, Environment environment, ProgramUnit parent) {
+class ListCreationExpression(val list: List<Expression>) : Expression {
+    override fun emit(instructions: InstructionsSet, environment: Environment, parent: ProgramUnit) {
         instructions.invokeConstructor(
-                ClassDesc.of("java.util.ArrayList"),
-                MethodTypeDesc.of(ConstantDescs.CD_void, List.of(ClassDesc.of("java.util.Collection"))),
-                argsInstructions -> {
-                    arrayListArgsInstructions(argsInstructions, environment);
-                    return Unit.INSTANCE;
-                }
-        );
+            ClassDesc.of("java.util.ArrayList"),
+            MethodTypeDesc.of(ConstantDescs.CD_void, ClassDesc.of("java.util.Collection"))
+        ) { arrayListArgsInstructions(environment) }
     }
 
-    @Override
-    public DataType getType(Environment environment, ProgramUnit parent) {
-        return DataType.ofNonNull(ClassDesc.of("java.util.ArrayList"));
+    override fun getType(environment: Environment, parent: ProgramUnit): DataType {
+        return DataType.ofNonNull(ClassDesc.of("java.util.ArrayList"))
     }
 
-    private void arrayListArgsInstructions(InstructionsSet instructionsSet, Environment environment) {
-        instructionsSet.invokeMethod(
-                ConstantDescs.CD_List,
-                "of",
-                MethodTypeDesc.of(ConstantDescs.CD_List, ConstantDescs.CD_Object.arrayType()),
-                InvokeType.STATIC_INTERFACE,
-                argsInstructions -> {
-                    ofArgsInstructions(argsInstructions, environment);
-                    return Unit.INSTANCE;
-                }
-        );
+    private fun InstructionsSet.arrayListArgsInstructions(environment: Environment) {
+        invokeMethod(
+            ConstantDescs.CD_List,
+            "of",
+            MethodTypeDesc.of(ConstantDescs.CD_List, ConstantDescs.CD_Object.arrayType()),
+            InvokeType.STATIC_INTERFACE
+        ) { ofArgsInstructions(environment) }
     }
 
-    private void ofArgsInstructions(InstructionsSet instructionsSet, Environment environment) {
-        instructionsSet.loadConstant(list.size());
-        instructionsSet.newReferenceArray(list.isEmpty() ? ConstantDescs.CD_Object : list.getFirst().getType(environment, this).getClassDesc());
+    private fun InstructionsSet.ofArgsInstructions(environment: Environment) {
+        loadConstant(list.size)
+        newReferenceArray(
+            if (list.isEmpty()) ConstantDescs.CD_Object
+            else list.first().getType(environment, this@ListCreationExpression).classDesc
+        )
 
-        for (int i = 0; i < list.size(); i++) {
-            Expression arg = list.get(i);
-            instructionsSet.duplicate();
-            instructionsSet.loadConstant(i);
-            arg.emit(instructionsSet, environment, this);
-            instructionsSet.storeReferenceIntoArray();
+        list.forEachIndexed { i, arg ->
+            duplicate()
+            loadConstant(i)
+            arg.emit(this, environment, this@ListCreationExpression)
+            storeReferenceIntoArray()
         }
     }
 }
--- src/main/java/me/itzisonn_/meazy/parser/ast/expression/collection_creation/MapCreationExpression.kt
+++ src/main/java/me/itzisonn_/meazy/parser/ast/expression/collection_creation/MapCreationExpression.kt
@@ -1,86 +1,59 @@
-package me.itzisonn_.meazy.parser.ast.expression.collection_creation;
+package me.itzisonn_.meazy.parser.ast.expression.collection_creation
 
-import kotlin.Unit;
-import lombok.Getter;
-import me.itzisonn_.meazy.instruction.InstructionsSet;
-import me.itzisonn_.meazy.instruction.method.InvokeMethodInstruction.InvokeType;
-import me.itzisonn_.meazy.parser.ast.ProgramUnit;
-import me.itzisonn_.meazy.parser.ast.expression.Expression;
-import me.itzisonn_.meazy.parser.DataType;
-import me.itzisonn_.meazy.runtime.environment.Environment;
-import org.jspecify.annotations.NullMarked;
+import me.itzisonn_.meazy.instruction.InstructionsSet
+import me.itzisonn_.meazy.instruction.method.InvokeMethodInstruction.InvokeType
+import me.itzisonn_.meazy.parser.DataType
+import me.itzisonn_.meazy.parser.ast.ProgramUnit
+import me.itzisonn_.meazy.parser.ast.expression.Expression
+import me.itzisonn_.meazy.runtime.environment.Environment
+import java.lang.constant.ClassDesc
+import java.lang.constant.ConstantDescs
+import java.lang.constant.MethodTypeDesc
 
-import java.lang.constant.ClassDesc;
-import java.lang.constant.ConstantDescs;
-import java.lang.constant.MethodTypeDesc;
-import java.util.List;
-import java.util.Map;
-
-@Getter
-@NullMarked
-public class MapCreationExpression implements Expression {
-    private final Map<Expression, Expression> map;
-
-    public MapCreationExpression(Map<Expression, Expression> map) {
-        this.map = map;
-    }
-
-    @Override
-    public void emit(InstructionsSet instructions, Environment environment, ProgramUnit parent) {
+class MapCreationExpression(val map: Map<Expression, Expression>) : Expression {
+    override fun emit(instructions: InstructionsSet, environment: Environment, parent: ProgramUnit) {
         instructions.invokeConstructor(
-                ClassDesc.of("java.util.HashMap"),
-                MethodTypeDesc.of(ConstantDescs.CD_void, List.of(ClassDesc.of("java.util.Map"))),
-                argsInstructions -> {
-                    hashMapArgsInstructions(argsInstructions, environment);
-                    return Unit.INSTANCE;
-                }
-        );
+            ClassDesc.of("java.util.HashMap"),
+            MethodTypeDesc.of(ConstantDescs.CD_void, ClassDesc.of("java.util.Map"))
+        ) { hashMapArgsInstructions(environment) }
     }
 
-    @Override
-    public DataType getType(Environment environment, ProgramUnit parent) {
-        return DataType.ofNonNull(ClassDesc.of("java.util.HashMap"));
+    override fun getType(environment: Environment, parent: ProgramUnit): DataType {
+        return DataType.ofNonNull(ClassDesc.of("java.util.HashMap"))
     }
 
-    private void hashMapArgsInstructions(InstructionsSet instructionsSet, Environment environment) {
-        instructionsSet.invokeMethod(
-                ConstantDescs.CD_Map,
-                "ofEntries",
-                MethodTypeDesc.of(ConstantDescs.CD_Map, ClassDesc.of("java.util.Map$Entry").arrayType()),
-                InvokeType.STATIC_INTERFACE,
-                argsInstructions -> {
-                    ofEntriesArgsInstructions(argsInstructions, environment);
-                    return Unit.INSTANCE;
-                }
-        );
+    private fun InstructionsSet.hashMapArgsInstructions(environment: Environment) {
+        invokeMethod(
+            ConstantDescs.CD_Map,
+            "ofEntries",
+            MethodTypeDesc.of(ConstantDescs.CD_Map, ClassDesc.of($$"java.util.Map$Entry").arrayType()),
+            InvokeType.STATIC_INTERFACE
+        ) { ofEntriesArgsInstructions(environment) }
     }
 
-    private void ofEntriesArgsInstructions(InstructionsSet instructionsSet, Environment environment) {
-        instructionsSet.loadConstant(map.size());
-        instructionsSet.newReferenceArray(ClassDesc.of("java.util.Map$Entry"));
+    private fun InstructionsSet.ofEntriesArgsInstructions(environment: Environment) {
+        loadConstant(map.size)
+        newReferenceArray(ClassDesc.of($$"java.util.Map$Entry"))
 
-        int i = 0;
-        for (Expression key : map.keySet()) {
-            Expression value = map.get(key);
-            int finalI = i;
+        map.entries.forEachIndexed { i, (key, value) ->
+            invokeMethod(
+                ClassDesc.of("java.util.Map"),
+                "entry",
+                MethodTypeDesc.of(
+                    ClassDesc.of($$"java.util.Map$Entry"),
+                    ConstantDescs.CD_Object,
+                    ConstantDescs.CD_Object
+                ),
+                InvokeType.STATIC_INTERFACE
+            ) {
+                duplicate()
+                loadConstant(i)
 
-            instructionsSet.invokeMethod(
-                    ClassDesc.of("java.util.Map"),
-                    "entry",
-                    MethodTypeDesc.of(ClassDesc.of("java.util.Map$Entry"), ConstantDescs.CD_Object, ConstantDescs.CD_Object),
-                    InvokeType.STATIC_INTERFACE,
-                    argsInstructions -> {
-                        argsInstructions.duplicate();
-                        argsInstructions.loadConstant(finalI);
+                key.emit(this, environment, this@MapCreationExpression)
+                value.emit(this, environment, this@MapCreationExpression)
+            }
 
-                        key.emit(argsInstructions, environment, this);
-                        value.emit(argsInstructions, environment, this);
-                        return Unit.INSTANCE;
-                    }
-            );
-
-            instructionsSet.storeReferenceIntoArray();
-            i++;
+            storeReferenceIntoArray()
         }
     }
 }
